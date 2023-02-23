a2b4a934fbf8e65b07f81b37645f0a057ad59bea
JFY1104 <119244967+JFY1104@users.noreply.github.com>
2023-02-23 22:52:55 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,26 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='*')
+bot = commands.Bot(command_prefix='*',intents=discord.Intents.all())
 
 @bot.event
+async def on_ready():
+    print("bot is online")
+
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(1078228435857588304)
+    await channel.send(f'{member} join!')
+   
+
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(1078228435857588304)
+    await channel.send(f'{member} leave!')
+
+
+bot.run('MTA3ODIzNzU4MjE0OTgyNDU5NA.GHTn_J.4vOvwbL11sQvF6HpUKkIjS1Qj9U5nTOrGWciwA')
 
 
